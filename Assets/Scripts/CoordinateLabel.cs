7dd4fe93bcd37b18bee5c647771ec3bcd7b6aa5c
artemmoskalev92 <83223168+artemmoskalev92@users.noreply.github.com>
2021-11-25 22:31:27 +0500

    Breadth First Search

--- Assets/Scripts/CoordinateLabel.cs
+++ Assets/Scripts/CoordinateLabel.cs
@@ -8,16 +8,19 @@ public class CoordinateLabel : MonoBehaviour
 
     [SerializeField] Color defaultColor = Color.black;
     [SerializeField] Color blockedColor = Color.gray;
+    [SerializeField] Color exploredColor = Color.yellow;
+    [SerializeField] Color pathColor = new Color(1f, 0.5f, 0f);
     TextMeshPro label;
     Vector2Int coordinates = new Vector2Int();
-    Waypoint waypoint;
+    GridManager gridManager;
 
 
     void Awake()
     {
+        gridManager = FindObjectOfType<GridManager>();
         label = GetComponent<TextMeshPro>();
         label.enabled = false;
-        waypoint = GetComponentInParent<Waypoint>();
+        
         DisplayCoordinates();
     }
 
@@ -43,13 +46,27 @@ public class CoordinateLabel : MonoBehaviour
 
     void SetLabelColor()
     {
-        if (waypoint.IsPlaceable)
+        if (gridManager == null) { return; }
+
+        Node node = gridManager.GetNode(coordinates);
+
+        if (node == null) { return; }
+
+        if (!node.isWalkable)
         {
-            label.color = defaultColor;
+            label.color = blockedColor;
+        }
+        else if (node.isPath)
+        {
+            label.color = pathColor;
+        }
+        else if (node.isExplored)
+        {
+            label.color = exploredColor;
         }
         else
         {
-            label.color = blockedColor;
+            label.color = defaultColor;
         }
     }
 
